f99106d3b6cc39763e3b26d466abcd3b23e598c2
Shun Ono <onoshun26@gmail.com>
2022-12-04 23:33:10 +0900

    done chapt 6

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
+mod logic;
 mod utils;
-mod mod logic;
 
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::{Clamped, JsCast};
@@ -35,7 +35,6 @@ macro_rules! measure_elapsed_time {
     }};
 }
 
-
 #[wasm_bindgen]
 pub fn generate_mandelbrot_set(
     canvas_w: usize,
@@ -45,9 +44,46 @@ pub fn generate_mandelbrot_set(
     y_min: f64,
     y_max: f64,
     max_iter: usize,
-    ) -> Vec<u8> {
+) -> Vec<u8> {
     measure_elapsed_time!("generate:wasm\telapsed:", {
-        logic::generate_mandelbrot_set(canvas_w, canvas_h,
-            x_min, x_max, y_min, y_max, max_iter)
+        logic::generate_mandelbrot_set(canvas_w, canvas_h, x_min, x_max, y_min, y_max, max_iter)
+    })
+}
+
+#[wasm_bindgen]
+pub fn draw_mandelbrot_set() {
+    const CANVAS_ID: &str = "canvas_wasm";
+    let document = web_sys::window().unwrap().document().unwrap();
+    let canvas = document.get_element_by_id(CANVAS_ID).unwrap();
+    let canvas: web_sys::HtmlCanvasElement = canvas
+        .dyn_into::<web_sys::HtmlCanvasElement>()
+        .map_err(|_| ())
+        .unwrap();
+    let context = canvas
+        .get_context("2d")
+        .unwrap()
+        .unwrap()
+        .dyn_into::<web_sys::CanvasRenderingContext2d>()
+        .unwrap();
+    let canvas_w = canvas.width() as usize;
+    let canvas_h = canvas.height() as usize;
+    const X_MIN: f64 = -1.5;
+    const X_MAX: f64 = 0.5;
+    const Y_MIN: f64 = -1.0;
+    const Y_MAX: f64 = 1.0;
+    const MAX_ITER: usize = 64;
+
+    let mut result = measure_elapsed_time!("generate:wasm\telapsed:", {
+        logic::generate_mandelbrot_set(canvas_w, canvas_h, X_MIN, X_MAX, Y_MIN, Y_MAX, MAX_ITER)
+    });
+    measure_elapsed_time!("draw:wasm\telapsed:", {
+        let data = ImageData::new_with_u8_clamped_array_and_sh(
+            Clamped(&mut result),
+            canvas.width(),
+            canvas.height(),
+        );
+        if let Ok(data) = data {
+            let _ = context.put_image_data(&data, 0.0, 0.0);
+        }
     })
 }
